cc4bc899fc214ca4f2eef575015acfb0517c47fd
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-04-03 20:53:33 +0100

    Update sysa.js

--- sysa.js
+++ sysa.js
@@ -1,1 +1,7 @@
+function sysa_open() {
+  document.getElementById("sysaSidebar").style.display = "block";
+}
 
+function sysa_close() {
+  document.getElementById("sysaSidebar").style.display = "none";
+}
