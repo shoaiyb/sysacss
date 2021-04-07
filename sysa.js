2636350875dcb9e4b42889630029dc848c1e81e3
shoaiyb sysa <75518689+shoaiyb@users.noreply.github.com>
2021-04-07 18:41:38 +0100

    Add files via upload

--- sysa.js
+++ sysa.js
@@ -1,7 +1,31 @@
-function sysa_open() {
-  document.getElementById("sysaSidebar").style.display = "block";
-}
+/* @package SYSA.JS */
+/* @author shoaiyb sysa */
+/* @license SYSA License */
 
-function sysa_close() {
-  document.getElementById("sysaSidebar").style.display = "none";
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("sysa-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4) {
+          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
+          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("sysa-include-html");
+          includeHTML();
+        }
+      } 
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
 }
